22cff7862b6347cf234c6bd14a86c2848679c645
Simon Marquis <simon.marquis@adevinta.com>
2026-08-14 15:07:03 +0200

    Reproducer for #1818

---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/ReproducerSpec.groovy
@@ -0,0 +1,27 @@
+// Copyright (c) 2026. Tony Robalik.
+// SPDX-License-Identifier: Apache-2.0
+package com.autonomousapps.jvm
+
+
+import com.autonomousapps.jvm.projects.ReproducerProject
+
+import static com.autonomousapps.utils.Runner.build
+import static com.google.common.truth.Truth.assertThat
+
+final class ReproducerSpec extends AbstractJvmSpec {
+
+  def "reproducer (#gradleVersion)"() {
+    given:
+    def project = new ReproducerProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, 'buildHealth')
+
+    then:
+    assertThat(project.actualProjectAdvice()).containsExactlyElementsIn(project.expectedProjectAdvice)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+}
